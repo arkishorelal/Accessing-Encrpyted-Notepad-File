075ff73f8eba1601434f8fea6b2477835892a337
Kishore lal <75961897+kishore-droid@users.noreply.github.com>
2021-08-09 18:40:16 +0530

    Add files via upload

---
+++ Project 1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include<fstream>
+//Creating password to text file and opening the text file
+using namespace std;
+int main()
+{
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE); 
+    DWORD mode = 0;
+    GetConsoleMode(hStdin, &mode);
+    SetConsoleMode(hStdin, mode & (~ENABLE_ECHO_INPUT));
+
+    string s,q;
+    int a;
+    cout<<"\t \t --------Encrypted mode--------- ";
+    cout<<"\nEnter your password "<<endl;
+    getline(cin, s);
+    cout<<"\n \nre enter your password to open the file "<<endl;
+    cin>>q;
+    if(s==q)
+    {
+    cout<<"\n \npassword sucessfully verified "<<endl;
+    ifstream ob;
+	ob.open("C:\\Users\\Kishore lal\\OneDrive\\Desktop\\example.txt", ios :: in);
+	if(ob.is_open())
+	{
+		string line;
+		cout<<"\n \n \t \t \t - - File content - - ";
+		while(getline(ob,line))
+		{
+			cout<<"\n \n "<<line<<endl;
+		}
+		ob.close();
+	}
+	}
+	else
+	{
+		cout<<"\n \nInvalid password, you can't open the file";
+	}
+    return 0;
+} 
